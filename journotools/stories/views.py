61013bb745e996b576e8b26b74cf5cc74757058f
eva tallaksen <eva.tallaksen@gmail.com>
2012-04-08 18:17:54 +0100

    Tidied up the Views file: removed white spaces and removed unnecessary old code. Also removed anything to do with the quotes/ transcripts.

--- journotools/stories/views.py
+++ journotools/stories/views.py
@@ -2,7 +2,7 @@ from stories.models import Article
 from stories.models import Source
 
 import json
-import datetime 
+import datetime
 
 from django.http import *
 from django.core.urlresolvers import reverse
@@ -12,7 +12,6 @@ from django.db.models import Q, Count
 from django.contrib.csrf.middleware import csrf_exempt
 from django.core.context_processors import csrf
 
-
 from django.contrib.auth.models import User
 from django.contrib.auth import *
 import django.contrib.auth.views
@@ -20,7 +19,6 @@ from django.contrib.auth.decorators import login_required
 
 status_dict = {'': 'To start', 'to_start':'To start', 'in_progress':'In progress', 'almost_done':'Almost done','completed':'Completed'}  
 category_dict = {'': 'News', 'news':'News', 'feature':'Feature', 'opinion':'Opinion','analysis':'Analysis'}  
-
 
 #**************************************************************     DUMMIES    ****************************************************************************
 #**************************************************************     DUMMIES    ****************************************************************************
@@ -30,7 +28,7 @@ category_dict = {'': 'News', 'news':'News', 'feature':'Feature', 'opinion':'Opin
 def test(request):
 	username = request.user.username
 	return render_to_response('dummies/test.html',{'User':username})
-	
+
 def users(request):
 	user_list = User.objects.all()
 	return render_to_response('dummies/users.html',{'user_list':user_list})
@@ -42,7 +40,7 @@ def users(request):
 def index(request):
 	return render_to_response('index.html',
 		context_instance=RequestContext(request))
-	
+
 #**************************************************************     USERS    ****************************************************************************
 #**************************************************************     USERS    ****************************************************************************
 #**************************************************************     USERS    ****************************************************************************
@@ -59,8 +57,7 @@ def register(request):
 	user = authenticate(username=u.username, password=request.POST['password'])
 	login(request, user)
 	return render_to_response('register.html',{'User':u},
-		context_instance=RequestContext(request))
-#register=csrf_exempt(register)
+		context_instance=RequestContext(request)) #Needed for the template to access the static files. 
 
 def login_submit(request):
 	user = authenticate(username=request.POST['username'], password=request.POST['password'])
@@ -75,7 +72,6 @@ def login_submit(request):
 	else:
 		message = "Your username and password were incorrect." 
 		return HttpResponseRedirect('/login_error')
-#login_submit=csrf_exempt(login_submit)
 
 def login_error(request):
 	return render_to_response('login_error.html',
@@ -94,28 +90,13 @@ def logout_view(request):
 #**************************************************************     ARTICLES    ****************************************************************************
 #**************************************************************     ARTICLES    ****************************************************************************
 
-'''@login_required(login_url='/login_page')
-def stories(request):
-	article_list = Article.objects.annotate(number_of_sources=Count('sources')).order_by('-pub_date')
-	order_by='-pub_date'
-	sort_by='desc'
-	if 'order_by' in request.GET and request.GET['order_by'] and request.GET['sort_by']:	
-		order_by = request.GET['order_by']
-		sort_by = request.GET['sort_by']
-		if request.GET['sort_by']=='desc':
-			article_list = article_list.order_by(order_by).reverse()
-		else:
-			article_list = article_list.order_by(order_by)
-	return render_to_response('stories.html', {'article_list':article_list,'order_by':order_by,'sort_by':sort_by,'status_dict':status_dict,'category_dict':category_dict})
-'''
 @login_required(login_url='/login_page')
 def stories(request):
 	if request.user.is_authenticated():
 		article_list = request.user.articles.annotate(number_of_sources=Count('sources')).order_by('-pub_date')
-		#article_list = Article.objects.filter(user=request.user).annotate(number_of_sources=Count('sources')).order_by('-pub_date')
 		order_by='-pub_date'
 		sort_by='desc'
-		if 'order_by' in request.GET and request.GET['order_by'] and request.GET['sort_by']:	
+		if 'order_by' in request.GET and request.GET['order_by'] and request.GET['sort_by']:
 			order_by = request.GET['order_by']
 			sort_by = request.GET['sort_by']
 			if request.GET['sort_by']=='desc':
@@ -124,8 +105,7 @@ def stories(request):
 				article_list = article_list.order_by(order_by)
 		return render_to_response('stories.html', {'article_list':article_list,'order_by':order_by,'sort_by':sort_by,'status_dict':status_dict,'category_dict':category_dict},
 			context_instance=RequestContext(request))
-		
-		
+
 @login_required(login_url='/login_page')
 def status_edit(request, article_id):
 	a = get_object_or_404(Article, pk=article_id)
@@ -169,7 +149,7 @@ def delete_submit(request, article_id):
 		return render_to_response('delete_submit.html',
 			context_instance=RequestContext(request))
 	else:
-		return HttpResponseRedirect(reverse('stories.views.stories', args=()))	
+		return HttpResponseRedirect(reverse('stories.views.stories', args=()))
 
 @login_required(login_url='/login_page')
 def add(request):
@@ -188,7 +168,6 @@ def add_submit(request):
 	a.notes = request.POST['notes']
 	a.category = request.POST['category']
 	a.status = request.POST['status']
-	#a.tags = request.POST['tags']
 	a.save()
 	b = request.POST['sources'].rsplit(',') #this takes the string and returns a list of the results in the string separated by the separator given -- here, the comma
 	list = []
@@ -202,27 +181,23 @@ def add_submit(request):
 		a.sources.add(s)
 	a.save()
 	article_id=a.id
-	#return HttpResponseRedirect(reverse('stories.views.stories', args=()))
 	if next == "stories":
 		return HttpResponseRedirect(reverse('stories.views.stories'))
 	else:
-		return HttpResponseRedirect(reverse('stories.views.%s' %next, kwargs={'article_id':article_id}))#return HttpResponseRedirect(reverse('stories.views.detail', kwargs={'article_id':article_id})) # the reverse tells the program to go to views.detail, tell it what url it is, then go back
-		
-	
+		return HttpResponseRedirect(reverse('stories.views.%s' %next, kwargs={'article_id':article_id}))
+		# the reverse tells the program to go to views.next, tell it what url it is, then go back to this program with the URL.
+		# If you skipped the reverse, it would go straight to the URL, which would make it go wonky if your URL changes.
+
 @login_required(login_url='/login_page')
 def add_source_api(request):
 	sources=json.loads(request.raw_post_data) # the json converter turns it from string to object. 
 	a = Source()
-	#q = Quote()
 	a.user = request.user
 	a.first_name = sources["first_name"]
 	a.last_name = sources["last_name"]
 	a.job_title = sources["job_title"]
 	a.company = sources["company"]
 	a.comments = sources["comments"]
-	# q.quote = sources["quotes"]
-	# q.save()
-	# a.quotes = q
 	a.save()
 	result = {}
 	result["id"] = a.id
@@ -246,8 +221,7 @@ def edit_submit(request, article_id):
 	a.status = request.POST['status']
 	a.notes = request.POST['notes']
 	a.category = request.POST['category']
-	#a.tags = request.POST['tags']
-	b = request.POST['sources'].rsplit(',') #this takes the string and returns a list of the results in the string separates by the separator given -- here, the comma
+	b = request.POST['sources'].rsplit(',') #takes the string and returns a list of the results separated by the separator given -- here, the comma
 	list = []
 	for i in range (0,len(b)-1):
 		if b[i] not in list:
@@ -261,9 +235,8 @@ def edit_submit(request, article_id):
 	if next == "stories":
 		return HttpResponseRedirect(reverse('stories.views.stories'))
 	else:
-		return HttpResponseRedirect(reverse('stories.views.%s' %next, kwargs={'article_id':article_id}))#return HttpResponseRedirect(reverse('stories.views.detail', kwargs={'article_id':article_id})) # the reverse tells the program to go to views.detail, tell it what url it is, then go back
-						#to this program with the URL. If you skipped the reverse, it would go straight to the URL, which would make it go wonky if your URL changes. 
-						
+		return HttpResponseRedirect(reverse('stories.views.%s' %next, kwargs={'article_id':article_id}))
+
 #**************************************************************     SOURCES  ****************************************************************************
 #**************************************************************     SOURCES  ****************************************************************************
 #**************************************************************     SOURCES  ****************************************************************************
@@ -271,7 +244,6 @@ def edit_submit(request, article_id):
 @login_required(login_url='/login_page')
 def sources_index(request):
 	source_list = request.user.sources.annotate(number_of_articles=Count('articles')).order_by('first_name')
-	#source_list = Source.objects.annotate(number_of_articles=Count('articles')).order_by('first_name')
 	order_by='first_name'
 	sort_by='desc'
 	if 'order_by' in request.GET and request.GET['order_by'] and request.GET['sort_by']:	
@@ -288,13 +260,9 @@ def sources_index(request):
 def sources_detail(request, source_id):
 	s = get_object_or_404(Source, pk=source_id)
 	article_list = s.articles.all()
-	#quote_list = s.quotes.all()
-	#short_quote_list = s.quotes.all()
-	#for quote in short_quote_list:
-	#	quote.content = quote.content[0:100]
 	return render_to_response('sources_detail.html', {'Source':s,'article_list':article_list},
-		context_instance=RequestContext(request)) # ,short_quote_list':short_quote_list
-		
+		context_instance=RequestContext(request))
+
 @login_required(login_url='/login_page')
 def sources_delete(request, source_id):
 	s = get_object_or_404(Source, pk=source_id)
@@ -308,7 +276,7 @@ def sources_delete_submit(request, source_id):
 	if request.POST['choice'] == "Yes":
 		s.delete()
 	return HttpResponseRedirect(reverse('stories.views.sources_index', args=()))
-	
+
 @login_required(login_url='/login_page')
 def sources_edit(request, source_id):
 	s = get_object_or_404(Source, pk=source_id)
@@ -325,7 +293,7 @@ def sources_edit_submit(request, source_id):
 	s.comments = request.POST['comments']
 	s.save()
 	return HttpResponseRedirect(reverse('stories.views.sources_index', args=()))
-	
+
 @login_required(login_url='/login_page')
 def sources_add(request):
 	return render_to_response('sources_add.html',
@@ -341,60 +309,12 @@ def sources_add_submit(request):
 	s.comments = request.POST['comments']
 	s.save()
 	return HttpResponseRedirect(reverse('stories.views.sources_index', args=()))
-	
+
 @login_required(login_url='/login_page')
 def source_query(request): 
 	query = request.GET['q']
-	source_list = request.user.sources.filter( # this is a python query to the database
+	source_list = request.user.sources.filter( #python query to the database
 		Q(first_name__istartswith=query) | Q(last_name__istartswith=query)
 	)
 	return render_to_response('source_query.json',{'source_list':source_list},
 		context_instance=RequestContext(request))
-
-#**************************************************************     TRANSCRIPTS  *****************************************************************************
-#**************************************************************     TRANSCRIPTS  *****************************************************************************
-#**************************************************************     TRANSCRIPTS  *****************************************************************************
-
-'''def quotes(request):
-	quote_list = Quote.objects.all()
-	short_quote_list = Quote.objects.all()
-	for quote in short_quote_list:
-		quote.content = quote.content[0:20]
-	return render_to_response('quotes.html', {'quote_list':quote_list,'short_quote_list':short_quote_list})
-
-def quote_detail(request, quote_id):
-	q = get_object_or_404(Quote, pk=quote_id)
-	return render_to_response('quote_detail.html', {'Quote':q})
-
-def quote_edit (request, quote_id):
-	q = get_object_or_404(Quote, pk=quote_id)
-	q.content = request.POST['transcript']
-	q.save()
-	return HttpResponseRedirect(reverse('stories.views.quote_detail', args=[quote_id]))
-
-quote_edit = csrf_exempt(quote_edit)
-	
-def new_quote(request):
-	return render_to_response('new_quote.html',
-		context_instance=RequestContext(request))
-	
-def submit_new_quote(request):
-	q = Quote()
-	q.content = request.POST['content']
-	source = request.POST['sources'][0]#this takes the string and returns a list of the results in the string separates by the separator given -- here, the comma
-	q.source = get_object_or_404(Source, pk = source)
-	q.save()
-	return HttpResponseRedirect(reverse('stories.views.quotes', args=()))	
-
-submit_new_quote = csrf_exempt(submit_new_quote)
-
-def submit_new_transcript(request): #this is used in the source detail template. 
-	q = Quote()
-	q.content = request.POST['transcript']
-	source = get_object_or_404(Source, pk=int(request.POST['source_id']))
-	q.source = source
-	q.save()
-	return HttpResponseRedirect(reverse('stories.views.sources_detail', args=[source.id]))
-	
-submit_new_transcript = csrf_exempt(submit_new_transcript)'''
-		
